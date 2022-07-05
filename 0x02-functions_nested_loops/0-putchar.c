6e95707a9a8ce5b2f8ae6e5c936a85c2c242875f
JezzeX <jezzejagz4@gmail.com>
2022-07-05 04:29:38 -0700

    Mandatory

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /*
- * main - Prints "_putchar" followed by a new line.
+ * main - Entry point
+ * 
+ * Description : 'This program prints '_putchar' followed by a new line
  *
  * Return: Always 0 (Success)
  */
