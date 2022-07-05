04e8beaf141773d602b5e9852943de0251c9171c
JezzeX <jezzejagz4@gmail.com>
2022-07-05 07:19:13 -0700

    Mandatory

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,14 @@
  */
 int main(void)
 {
-	char sigh[9] = "_putchar";
-	int i;
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(sigh[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
