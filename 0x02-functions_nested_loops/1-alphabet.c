6d7715e143dc2051731d38d5a2331108e455efd6
JezzeX <jezzejagz4@gmail.com>
2022-07-05 07:24:53 -0700

    Mandatory

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet in lowercase
+ *
+ * Return : ALways void
+ *
+ */
+
+void print_alphabet(void)
+{
+	int a;
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
