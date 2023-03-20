ad41a3c69540a2ebcc6641e6d651bf1bfdadabc3
JezzeX <jezzejagz4@gmail.com>
2023-03-20 13:26:33 +0100

    Task 4 solved

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+  * main - prints all letters of the alphabet except q and e
+  * Return: return 0
+  */
+
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		if ( abc != 'e' && abc != 'q')
+		{
+			putchar(abc);
+		}
+	}
+	return (0);
+}
