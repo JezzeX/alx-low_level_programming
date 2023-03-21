4848389577c9cf74922ca95a5dd965dbfc92c8d5
JezzeX <jezzejagz4@gmail.com>
2023-03-21 17:22:19 +0100

    Task 6 solved

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _abs - prints absolute value of integer
+  * @n: the integer to check
+  * Return: absolute value or negative value
+  */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
