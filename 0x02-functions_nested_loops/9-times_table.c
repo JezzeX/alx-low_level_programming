1770eac0c1f5d3b451832ccd48d71b8d4d8e0784
JezzeX <jezzejagz4@gmail.com>
2023-03-21 18:03:35 +0100

    Task 9 solved

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,33 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, k, l;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-
-			_putchar((i * j) + '0');
-			_putchar(',');
-			_putchar(' ');
+			k = i * j;
+			if ( k == 0)
+			{
+				_putchar(k + '0');
+			}
+			else if (k > 9)
+			{
+				l = k % 10;
+				k /= 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k + '0');
+				_putchar(l + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
 		}
 		_putchar('\n');
 	}
