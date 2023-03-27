83d3bdc3f5f8a9be4069976761c2f489ab072ce9
JezzeX <jezzejagz4@gmail.com>
2023-03-27 20:06:21 +0100

    Task 5 solved

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * rev_string - function to reverse a string
+  * @s: string
+  */
+
+void rev_string(char *s)
+{
+	char tempstr[50];
+	int i, j, k;
+	i = 0;
+	j = 0;
+	k = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	i--;
+
+	while (i >= 0)
+	{
+		tempstr[j] = s[i];
+		j++;
+		i--;
+	}
+
+	while (k <= j)
+	{
+		s[k] = tempstr[k];
+		k++;
+	}
+}
