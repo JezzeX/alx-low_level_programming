3fb01757adac79ee85db046d2eeb8a4d97bde478
JezzeX <jezzejagz4@gmail.com>
2023-03-27 21:14:39 +0100

    Task 7 solved

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * puts_half - prints half of the string
+  * @str: string
+  */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	i--;
+
+	if ((i % 2) == 0)
+	{
+		k = i / 2;
+		k++;
+		while (k <= i)
+		{
+			_putchar(str[k]);
+			k++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		k = (i - 1) / 2;
+		
+		while (k <= i)
+		{
+			_putchar(str[k]);
+			k++;
+		}
+		_putchar('\n');
+	}
+}
