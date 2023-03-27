2d36f4038535b21d0c5a67e7052b589d253ffad4
JezzeX <jezzejagz4@gmail.com>
2023-03-27 19:16:52 +0100

    Task 3 solved

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -3,10 +3,10 @@
 /**
   * _putchar - writes the charcter c to stdout
   * @c: character
-  * Return: 1
+  *
+  * Return: On success 1
   * On error, -1 is returned, and errno is set appropriately
   */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
