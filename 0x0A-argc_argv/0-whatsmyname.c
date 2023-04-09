38a0ed25cc3349807c5711484ca897001c205092
JezzeX <jezzejagz4@gmail.com>
2023-04-09 13:00:04 +0100

    Task 0 solved

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - function that prints its name followed by new line
+  * @argc: the size of the argv array
+  * @argv: an array containing the program command line args
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
