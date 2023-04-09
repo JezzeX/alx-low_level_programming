b367105521192911e3efde417726677aa1057acc
JezzeX <jezzejagz4@gmail.com>
2023-04-09 13:31:27 +0100

    Task 1 solved

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - print number followed by new line
+  * @argv: the number to print
+  * @argc: the number
+  * Return: 0
+  */
+
+int main(int argc, char *arg[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
